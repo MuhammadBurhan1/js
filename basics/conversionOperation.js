066bc5a75f74740aa0b640efb1a8a8e259a14ec6
MuhammadBurhan1 <128923339+MuhammadBurhan1@users.noreply.github.com>
2024-07-30 10:20:01 +0000

    conversionOperations

---
+++ basics/conversionOperation.js
@@ -0,0 +1,25 @@
+"use strict";// treat all JS code as newer version
+let score = "Muhammad Burhan";
+//console.log(typeof score);
+//console.log(typeof (score));
+const valueInNumber= Number(score);
+//console.log(valueInNumber);  
+//console.log(typeof valueInNumber);
+//console.log(typeof NaN);// So,here we have come to known that the type of NaN is a number.
+//"33"=>33;
+// null=>NaN // NaN stands for not a number whose datatype is number.
+// boolean=>1 is for true , and 0 is for false
+// undefined=>NaN 
+let isLoggedIn= "Burhan";
+const conversion=Boolean(isLoggedIn);
+//console.log(typeof conversion);
+//console.log(conversion);
+// 1=>true
+// 0=>false
+// ""=>false
+// "Burhan"=>true
+
+let number=33;
+const stringNumber= String(number);
+console.log(stringNumber);
+console.log(typeof stringNumber);
