e7e156bfe5fd960dd31762a6bb2e39b44eac461e
MuhammadBurhan1 <128923339+MuhammadBurhan1@users.noreply.github.com>
2024-07-30 09:07:07 +0000

    updated variable.js

---
+++ basics/variables.js
@@ -0,0 +1,16 @@
+const accountId=18290;
+let city="lahore";
+var email="burhanmalik813@gmail.com";
+province="Punjab";
+// accountId=2; // const variable cannot be changed
+// console.log(accountId);
+// city="Islamabad"; // let variable can be changed 
+// console.log(city);
+// console.log(province);
+// console.log(email); 
+// email="normal@gmail.com";//Prefer not use var variable because of block scoping and functional scoping.
+// console.log(email);
+let number;//Undefined variable can be assigned to let
+// const number1; // undefined value cannot be assigned to const variable
+console.table([accountId,city,email,province,number]);
+
