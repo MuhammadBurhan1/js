d1df251b57653636df96b3ded8d56604bd3487dd
MuhammadBurhan1 <128923339+MuhammadBurhan1@users.noreply.github.com>
2024-07-30 11:11:14 +0000

    updated conversionOperation.js

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -21,5 +21,41 @@ const conversion=Boolean(isLoggedIn);
 
 let number=33;
 const stringNumber= String(number);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(typeof stringNumber);
+//console.log(stringNumber);
+
+//Operations
+let value = 3;
+let negValue = -value;
+console.log(negValue); 
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);
+//console.log(2/2);
+//console.log(2%2);
+let str1 = "hello";
+let str2 = " Burhan ";
+//let str3 = str1 + str2;
+////console.log(str3);
+////console.log("1" + 2);//12
+////console.log(1 + "2");//12
+////console.log("1" +2 +2);//122
+//console.log(1+2+"2");//32
+// console.log(true);
+// console.log(typeof true); 
+// console.log(+true);
+// console.log(typeof +true);
+// console.log(+"");
+// console.log(typeof +"");
+
+//let num1=num2=num3;
+//num1=num2=num3=2+2;
+//console.log(num1=num2=num3=2+2);
+let gameCounter = 100;
+gameCounter++; //gameCounter=gameCounter+1; that is an example of postfix operator So, in postfix operator,the increment operator returns the value before incrementing.
+++gameCounter;//gameCounter+1=gameCounter ; that is an example of prefix operator so, in prefix operator the increment operator returns the value after incrementing. 
+console.log(gameCounter);
+
+
+
